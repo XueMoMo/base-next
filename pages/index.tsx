8c30654aa31e75fa22eccea55080bdb39d3a9e87
eericxu <2681350846@qq.com>
2021-10-13 11:02:08 +0800

    init base next

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,31 @@
 import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
-import { GetStaticProps } from 'next'
+import Layout, {siteTitle} from '../components/layout'
+import {Image} from "semantic-ui-react";
+import styles from '../styles/index.module.scss';
+import classNames from "classnames";
+import {useTranslation} from "react-i18next";
+import BgAnim from '../components/effect/BgAnim';
+import useParallax from "../lib/hooks/useParallax";
 
-export default function Home({
-  allPostsData
-}: {
-  allPostsData: {
-    date: string
-    title: string
-    id: string
-  }[]
-}) {
+export default function Home({}: {}) {
+  const {t} = useTranslation()
+  const {data} = useParallax(120, 5)
+
   return (
-    <Layout home>
+    <Layout className={styles.main}>
+      <BgAnim/>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
+
+      <div style={{
+        overflow: "hidden"
+      }}>
+        {data.map((ig, i) =>
+          <Image circular src={"/images/profile.jpg"} size={'tiny'} inline key={`image_${i}`}
+                 className={classNames(styles.animStart, {[styles.animFinal]: ig.value})}/>
+        )}
+      </div>
     </Layout>
   )
-}
-
-export const getStaticProps: GetStaticProps = async () => {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
 }
